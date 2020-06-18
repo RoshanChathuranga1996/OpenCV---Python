f6d8e9ac07583ff31eb7f6cda593785ee348852b
Roshan Chathuranga <48976976+RoshanChathuranga1996@users.noreply.github.com>
2020-06-18 15:49:48 +0530

    Add files via upload

---
+++ testVid.py
@@ -0,0 +1,113 @@
+from imutils import paths
+import time
+import cv2
+import numpy as np
+import pickle
+import vptree
+from matplotlib import pyplot as plt
+
+
+def dhash(image, hashsize=8):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(image, (hashsize + 1, hashsize))
+    diff = resized[:, 1:] > resized[:, -1:]
+    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
+
+def convert_hash(h):
+    return int(np.array(h, dtype='float64'))
+
+
+def hamming(a, b):
+    return bin(int(a) ^ int(b)).count('1')
+
+
+capture = cv2.VideoCapture('img/Megamind.avi')
+start = time.time()
+hashes = {}
+i = 0
+
+while True:
+    ret, frame = capture.read()
+
+    if ret:
+
+        cv2.imwrite('C:\\Users\\Roshan\\Desktop\\New folder\\FrameWrite\\Frame_' + str(i) + '.jpg', frame)
+        print("Processing Frame_{}".format(str(i)))
+        imagePath = 'C:\\Users\\Roshan\\Desktop\\New folder\\FrameWrite\\Frame_{}.jpg'.format(str(i))
+        i += 1
+
+        h = dhash(frame)
+        h = convert_hash(h)
+
+        update = hashes.get(h, [])
+        update.append(imagePath)
+        hashes[h] = update
+
+    else:
+        break
+
+capture.release()
+print(hashes)
+
+end = time.time()
+print('--------' * 7)
+print("[INFO] Loading video took {} seconds".format(end - start))
+
+print('--------' * 7)
+print("[INFO] building VP-Tree...")
+points = list(hashes.keys())
+tree = vptree.VPTree(points, hamming)
+
+print("[INFO] serializing VP-Tree...")
+f = open('C:\\Users\\Roshan\\Desktop\\New folder\\vptree_vid.pickle', 'wb')
+f.write(pickle.dumps(tree))
+f.close()
+
+print("[INFO] serializing hashes...")
+f = open('C:\\Users\\Roshan\\Desktop\\New folder\\hashes_vid.pickle', 'wb')
+f.write(pickle.dumps(hashes))
+f.close()
+
+
+# Search for image
+print("[INFO] loading VP-Tree and hashes...")
+tree = pickle.loads(open('C:\\Users\\Roshan\\Desktop\\New folder\\vptree_vid.pickle', 'rb').read())
+hashes = pickle.loads(open('C:\\Users\\Roshan\\Desktop\\New folder\\hashes_vid.pickle', 'rb').read())
+
+searchImg = cv2.imread('img/Frame_44.jpg')
+
+searchHash = dhash(searchImg)
+searchHash = convert_hash(searchHash)
+print('Search image hash = ' + str(searchHash))
+
+print('--------' * 7)
+print("[INFO] performing search...")
+start = time.time()
+results = tree.get_all_in_range(searchHash, max_distance=10)
+results = sorted(results)
+
+
+for (d, h) in results:
+    resultsPaths = hashes.get(h, [])
+    print("[INFO] {} total image(s) with d: {}, h: {}".format(len(resultsPaths), d, h))
+
+    for resultsPath in resultsPaths:
+        if d < 6:
+            result = cv2.imread(resultsPath)
+            font = cv2.FONT_HERSHEY_PLAIN
+            txt = "Difference: {},    ImageHash: {}".format(d, h)
+            #result = cv2.putText(result, txt, (20, 30), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+
+            cv2.namedWindow("SampleImg", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("SampleImg", 550, 400)
+            cv2.imshow("SampleImg", searchImg)
+
+            plt.figure(figsize=(10, 6))
+            plt.title(txt)
+            plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+            plt.show()
+
+    end = time.time()
+    print("[INFO] search took {} seconds".format(end - start))
+    print('--------' * 7)
