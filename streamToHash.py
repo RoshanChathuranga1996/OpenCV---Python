c912f21b2711e3752cb231cbc1e8d730043e0040
Roshan Chathuranga <48976976+RoshanChathuranga1996@users.noreply.github.com>
2021-02-09 17:12:33 +0530

    Update streamToHash.py

--- streamToHash.py
+++ streamToHash.py
@@ -6,13 +6,15 @@ from datetime import datetime
 from func import dHash, convert_hash
 
 
+imagesArray = []
 streamFrameNo = 0
 # Stream
 path = "E:\\Opencv_project\\Videos\\Stream\\d2.mp4"
 capture = cv2.VideoCapture(0)
 
+
 while True:
-    global streamFld, endSF, startSF, t, configLoc
+    global streamFld, endSF, startSF, t, configLoc, width, height
     ret, frame = capture.read()
 
     if ret:
@@ -26,10 +28,13 @@ while True:
         print("-- Processing frame {} --".format(streamFrameNo))
 
         now = datetime.now()
+        nowTime = now.time()
+        nowTime = str(nowTime.strftime("%H-%M-%S-%f"))
         nowStr = datetime.date(datetime.now()).strftime("%Y%b%d")
         nowLoc = str(now.date()) + " " + str(now.hour)
         ff = "{}".format(nowLoc)
-        SFld = r'E:\\Opencv_project\\StreamTxt\\{}'.format(nowStr)  # path
+
+        SFld = r'E:\\Opencv_project\\StreamHash\\{}'.format(nowStr)  # path
         if not os.path.exists(SFld):
             os.makedirs(SFld)
 
@@ -37,15 +42,16 @@ while True:
         file_h.write(str(datetime.now().time()) + " " + str(streamFrameNo) + " " + "{} {}\n".format(check, checkD))
         file_h.close()
 
-        stream = r'E:\\Opencv_project\\Frames\\Set01\\Stream'  # path
+        stream = r'E:\\Opencv_project\\Frames\\Stream\\{}\\{}'.format(nowStr, ff)
         if not os.path.exists(stream):
             os.makedirs(stream)
 
-        # txtAd = "Hash = {}".format(check)
-        # font = cv2.FONT_HERSHEY_SIMPLEX
-        # frame = cv2.putText(frame, txtAd, (40, 40), font, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
-        # cv2.imwrite(stream + "\\" + str(streamFrameNo) + '.jpg', frame)
+        resized_image = cv2.resize(frame, (100, 50))
+        gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(stream + "\\" + nowTime + '.jpg', gray_image)
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         streamFrameNo += 1
 
     else:
